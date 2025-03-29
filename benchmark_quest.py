e1a5085aaddf8699da7d0add31b58eb6bd5ea5a6
lwh <rainfromcn@gmail.com>
2025-03-29 20:53:41 +0800

    update

--- benchmark_quest.py
+++ benchmark_quest.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
     parser.add_argument("--page_size", type=int, default=16)
     parser.add_argument("--token_budget", type=int, default=128)
     parser.add_argument("--num_repeats", type=int, default=3)
+    parser.add_argument("--chunk_prefill", type=int, default=True)
     args = parser.parse_args()
 
     benchmark_sketch = WallTime("step-1", cuda=0)
@@ -31,35 +32,38 @@ if __name__ == '__main__':
         head_dim=args.head_dim, 
         page_size=args.page_size, 
         page_budget=page_budget,
-        max_seq_len=1024 * 512,
+        max_seq_len=1024 * 1024,
         dtype=torch.float16,
         device='cuda:0')
 
-    # 计算 LSH 相似度
-    for ctx_length in [1024 * (2 ** i) for i in range(10)]:        
 
-        for _ in range(args.num_repeats):
-            iController.prepare_metadata(ctx_length)
+    with torch.no_grad():
+        for ctx_length in [1024 * (2 ** i) for i in range(11)]:        
 
-            # 1.pre-filling stage
-            iController.begin_forward(ctx_length)
-            hidden_states = torch.rand((args.batch_size, ctx_length, args.num_heads * args.head_dim), dtype=torch.float16).cuda()
-            attn(hidden_states, use_cache=True, iController=iController)
-            iController.end_forward()
+            for _ in range(args.num_repeats):
+                iController.prepare_metadata(ctx_length)
 
-            # 2.decoding stage
-            iController.begin_forward(1)
-            hidden_states = torch.rand((args.batch_size, 1, args.num_heads * args.head_dim), dtype=torch.float16).cuda()
-            attn(hidden_states, use_cache=True, iController=iController)
-            iController.end_forward()
-            iController.clean_states()
+                # 1.pre-filling stage, this part is not involved in latency test
+                for start in range(0, ctx_length, 1024):
+                    end = min(ctx_length, start + 1024)
+                    iController.begin_forward(end - start)
+                    hidden_states = torch.rand((args.batch_size, end - start, args.num_heads * args.head_dim), dtype=torch.float16).cuda()
+                    attn(hidden_states, use_cache=True, iController=iController)
+                    iController.end_forward()
 
-        benchmark_sketch.result(postfix=f'-{ctx_length}')
-        benchmark_topk.result(postfix=f'-{ctx_length}')
-        benchmark_sparse.result(postfix=f'-{ctx_length}')
+                # 2.decoding stage
+                iController.begin_forward(1)
+                hidden_states = torch.rand((args.batch_size, 1, args.num_heads * args.head_dim), dtype=torch.float16).cuda()
+                attn(hidden_states, use_cache=True, iController=iController)
+                iController.end_forward()
+                iController.clean_states()
 
-        benchmark_sketch.reset()
-        benchmark_topk.reset()
-        benchmark_sparse.reset()
+            benchmark_sketch.result(postfix=f'-{ctx_length}')
+            benchmark_topk.result(postfix=f'-{ctx_length}')
+            benchmark_sparse.result(postfix=f'-{ctx_length}')
 
-        print("\n")
+            benchmark_sketch.reset()
+            benchmark_topk.reset()
+            benchmark_sparse.reset()
+
+            print("\n")
